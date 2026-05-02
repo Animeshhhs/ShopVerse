cf6009c6b3d673756ac00392f53aa5c3526bb0db
Animeshhhs <itsanimeshs@gmail.com>
2026-05-02 17:07:58 +0530

    Delete Shopverse/Sql_val directory

--- Shopverse/Sql_val/address.sql
+++
@@ -1,3 +0,0 @@
-insert into e_commerce.address values (1,'108','meow industries','dev nagar','punjab','patiala','147004',1);
-insert into e_commerce.address values (2,'214/B','laxmi enterprises','rawalpindi','punjab','patiala','147004',2);
-insert into e_commerce.address values (3,'52','oberoi traders','thakur pratap nagar','punjab','patiala','147004',3);
